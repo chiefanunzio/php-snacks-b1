a200cc724d3aa16a6cd9739f9f411d0cbc620609
chiefanunzio <chiefa.nunzio@gmail.com>
2021-05-04 22:11:24 +0200

    fix array + ex 2 + css

--- index.php
+++ index.php
@@ -9,82 +9,100 @@
 </head>
 
 <style>
-body{
-    
-}
+    body {
+        background-color: blue;
+        text-align:center;
+    }
 
-    .box{
-        
-        height:20px;
+    .box {
+        font-size:30px;
+        border:1px solid blue;
         background-color: #ccc;
     }
 
-    .res{
+    .res {
         ;
-        height:20px;
+        height: 20px;
         background-color: green;
 
+    }
+
+    h1{
+        background-color: purple;
+        padding: 10px;
+        border-radius:5px;
+        width:30%;
+        margin:20px auto;
+    }
+
+    .error{
+        color:red;
+    }
+
+    .valid{
+        color:green;
     }
 
 </style>
 
 <body>
 
-        <?php
+    <?php
             $torneo = [
                 'giornata-1' => [
                     [
                         'casa' => 'milano',
                         'ospite' => 'cantu',
-                        'punteggio' => '55-60'
+                        'punteggio-casa' => 55,
+                        'punteggio-ospite'=> 60
                     ],
-                    
-                    
-                ],
-                'giornata-2' => [
                     [
                         'casa' => 'taranto',
                         'ospite' => 'catania',
-                        'punteggio' => '74-70'
-                        
+                        'punteggio-casa' => 70,
+                        'punteggio-ospite'=> 74
                     ],
-                ]
+                    [
+                        'casa' => 'roma',
+                        'ospite' => 'torino',
+                        'punteggio-casa' => 80,
+                        'punteggio-ospite'=> 54
+                        ]
+                ],
             ];
-
-            $giornata1 = $torneo['giornata-1'];
-            $giornata2 = $torneo['giornata-2'];
-                   
                     
-                    for ($i=0;$i<count($giornata1);$i++) {
-                        $res1 = $giornata1[$i];
-                        echo '<div class="box">' 
-                            . $res1['casa'] . ' - ' 
-                            . $res1['ospite'] . ' - ' 
-                            . $res1['punteggio']
-                            .'</div>'; 
-                           
-                        
-                           
-                        }
-                    for ($i=0;$i<count($giornata2);$i++) {
-                        $res2 = $giornata2[$i];
-                        echo '<div class="box">' 
-                            . $res2['casa'] . ' - ' 
-                            . $res2['ospite'] . ' - ' 
-                            . $res2['punteggio']
-                            .'</div>'; 
-                           
-                    }
+            $giornata1 = $torneo['giornata-1'];
             
+            for ($i=0;$i<count($giornata1);$i++) {
+                $res1 = $giornata1[$i];
+                echo '<div class="box">' 
+                    . $res1['casa'] . ' - ' 
+                    . $res1['ospite'] . ' - | ' 
+                    . $res1['punteggio-casa'] . ' - '
+                    . $res1['punteggio-ospite']
+                    .'</div>'; 
 
+                }
+        ?>
+    
+        <?php
+
+
+            $name = $_GET['name'] ;
+            $mail = $_GET['email'];
+            $age = $_GET['age'];
+            $find = strpos($mail, '@');
+            $find2 = strpos($mail, '.');
+            $isInt = is_numeric($age);
+            if(strlen($name) < 3 || $find === false || $find2 === false || $isInt === false){
+                echo '<h1 class="error">' . 'accesso negato' . '</h1>';
+            }
+            else{
+                echo '<h1 class="valid">' . 'accesso riuscito' . '</h1>';
+            }
 
         ?>
     
-
-
-
-
-
 
 
 </body>
